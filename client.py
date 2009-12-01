3bcb6b145d706d30c2a27279b1c323a2e2d739b5
Dragon <sdrinf@gmail.com>
2009-12-01 09:49:44 +0000

    python headers

--- client.py
+++ client.py
@@ -1,4 +1,7 @@
-# command line tools for the waveclient
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# Copyright @ 2009, Waverz.com - BSD license - Information wants to be free!
+# Command line tools for the waveclient
 
 import os
 import sys
@@ -7,44 +10,44 @@ from wirewave import WaveReader
 
 
 def viewwave(wid):
-    print "rendering "+wid
-    w = WaveReader()
-    w.read(wid)
-    w.render()
-    (h,fn) = tempfile.mkstemp(".html","wave")
-    os.write(h,w.renderedHTML.encode("UTF8"))
-    os.close(h)
-    os.startfile(fn)
-    pass
+	print "rendering "+wid
+	w = WaveReader()
+	w.read(wid)
+	w.render()
+	(h,fn) = tempfile.mkstemp(".html","wave")
+	os.write(h,w.renderedHTML.encode("UTF8"))
+	os.close(h)
+	os.startfile(fn)
+	pass
 
 def showcode(wid):
-    w = WaveReader()
-    w.read(wid)
-    code = w.get_root_text()
-    code = code.replace("\n","\r\n")
-    print code
-    (h,fn) = tempfile.mkstemp(".txt","wave")
-    os.write(h,code.encode("UTF8"))
-    os.close(h)
-    os.startfile(fn)
-    pass
+	w = WaveReader()
+	w.read(wid)
+	code = w.get_root_text()
+	code = code.replace("\n","\r\n")
+	print code
+	(h,fn) = tempfile.mkstemp(".txt","wave")
+	os.write(h,code.encode("UTF8"))
+	os.close(h)
+	os.startfile(fn)
+	pass
 
 def printhelp():
-    print """Usage: client.py [command] [arguments]
+	print """Usage: client.py [command] [arguments]
 Usable wcmd commands:
-   view [waveid]\t-Displays a wave in default browser
-   showtext [waveid]\t-Displays text content only
+	      view [waveid]\t-Displays a wave in default browser
+	      showtext [waveid]\t-Displays text content only
 """
-    
+
 if (len(sys.argv) < 2):
-    printhelp()
-    exit()
-    pass
+	printhelp()
+	exit()
+	pass
 if (sys.argv[1] == "--help"):
-    printhelp()
+	printhelp()
 elif (sys.argv[1] == "showcode"):
-    showcode(sys.argv[2])
+	showcode(sys.argv[2])
 elif (sys.argv[1] == "view"):
-    viewwave(sys.argv[2])
+	viewwave(sys.argv[2])
 else:
-    print "client: '"+sys.argv[1]+"' is not a valid command. See 'client.py --help'"
+	print "client: '"+sys.argv[1]+"' is not a valid command. See 'client.py --help'"
---
+++ wireutils.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# Copyright @ 2009, Waverz.com - BSD license - Information wants to be free!
+
