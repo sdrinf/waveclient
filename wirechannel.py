b1657627f37e8bd954876d3479c6141bffd01e7d
Dragon <sdrinf@gmail.com>
2009-12-02 02:13:22 +0000

    wirechannel reading started

--- wirechannel.py
+++ wirechannel.py
@@ -1,4 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# Copyright @ 2009, Waverz.com - BSD license - Information wants to be free!
+
 # direct wave wire channel implementation:
 # reads the public stream
 
+import random
+from wireutils import webrequest
+from waveapi import simplejson
 
+def refresh_search():
+    session = "503282974482375274562"
+    cookie = "DQAAAI8AAADqGuAdyCS0oaSk8PE4FDZZUl_KCmr0Gj6AVICpeE9WScTxIjM5CJLrNI23neGAQtXEDs51NkaLaxoik-IRUyLxwpzwa5Q3sUeRJ9Xo1Zh-6bpZlckaS242zuacV_Fk7qLULp2JRf1I5YbFalpolOU2_BzyBenDi9pf_22hhBJWQwXWm9c4YYQOlynF2fLI08Q"
+    rid = random.randint(0, 100000)
+    zx = "".join([chr(random.randint(97,122)) for i in xrange(0, 11)])
+    resdat = webrequest("POST", "wave.google.com", 
+                        "/wave/wfe/channel?VER=6&RID="+str(rid)+
+                        "&CVER=3&zx="+zx+"&t=1",
+                        {'Cookie': "WAVE="+cookie},
+                        {"count" : "0"} )
+    print resdat
+
+
+def refresh_searchpublic():
+    session = "503282974482375274562"
+    cookie = "DQAAAI8AAADqGuAdyCS0oaSk8PE4FDZZUl_KCmr0Gj6AVICpeE9WScTxIjM5CJLrNI23neGAQtXEDs51NkaLaxoik-IRUyLxwpzwa5Q3sUeRJ9Xo1Zh-6bpZlckaS242zuacV_Fk7qLULp2JRf1I5YbFalpolOU2_BzyBenDi9pf_22hhBJWQwXWm9c4YYQOlynF2fLI08Q"
+    rid = random.randint(0, 100000)
+    zx = "".join([chr(random.randint(97,122)) for i in xrange(0, 11)])
+    resdat = webrequest("POST", "wave.google.com", 
+                        "/wave/wfe/channel?VER=6&RID="+str(rid)+
+                        "&CVER=3&zx="+zx+"&t=1",
+                        {'Cookie': "WAVE="+cookie},
+                        {"count" : "2",
+                         "req0_key" : '{"a":"RY3Rrawn","r":"e","t":2007,"p":{"1000":[0,0],"2":"RY3Rrawn0"}}',
+                         "req1_key" : '{"a":"RY3Rrawn","r":"f","t":2602,"p":{"1000":[0,0],"2":"RY3Rrawn2","3":"","4":{"2":25,"1":0},"6":"public@a.gwave.com"}}'} )
+    print resdat
+    wiredata = "".join( resdat.split("\n")[1:])
+    csid = wiredata[10:26]
+    print "sid: "+csid
+    zx = "".join([chr(random.randint(97,122)) for i in xrange(0, 11)])
+    resdat = webrequest("GET", "wave.google.com", 
+                        "/wave/wfe/channel?VER=6&RID=rpc&SID="+csid+
+                        "&CI=0&AID=99&TYPE=xmlhttp&zx="+zx+"&t=1",
+                        {'Cookie': "WAVE="+cookie},
+                        None)
+    print resdat
+    pass
+
+print refresh_search()
--- wireutils.py
+++ wireutils.py
@@ -2,3 +2,35 @@
 # -*- coding: UTF-8 -*-
 # Copyright @ 2009, Waverz.com - BSD license - Information wants to be free!
 
+# misc utilities
+
+def webrequest(method, host, url, headers, payload):
+    """Request an HTTPS page, either via the appengine api, or using httplib"""
+    import urllib
+    
+    def fetch_urllib2(method, host, url, headers, payload):
+        """use urllib2 to fetch a wave"""
+        import httplib
+        connection = httplib.HTTPSConnection(host)
+        connection.request(method, url, payload, headers)
+        http_response = connection.getresponse() 
+        http_data = http_response.read()
+        connection.close()
+        return http_data
+
+    if (isinstance(payload, dict)):
+        payload = urllib.urlencode(payload)
+    if ((method == "POST")) & (not ("Content-Type" in headers)):
+        headers['Content-Type'] = 'application/x-www-form-urlencoded'
+    if not ("User-Agent" in headers):
+        headers['User-Agent'] = "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/525.13 (KHTML, like Gecko) Chrome/4.0.249.11 Safari/525.13"
+    return fetch_urllib2(method,host,url,headers,payload)
+
+
+
+if (__name__ == "__main__"):
+    session = "503282974482375274562"
+    cookie = "DQAAAI8AAADqGuAdyCS0oaSk8PE4FDZZUl_KCmr0Gj6AVICpeE9WScTxIjM5CJLrNI23neGAQtXEDs51NkaLaxoik-IRUyLxwpzwa5Q3sUeRJ9Xo1Zh-6bpZlckaS242zuacV_Fk7qLULp2JRf1I5YbFalpolOU2_BzyBenDi9pf_22hhBJWQwXWm9c4YYQOlynF2fLI08Q"
+    waveid = "googlewave.com!w+fkvUXIemA"
+    print webrequest("GET", "wave.google.com", "/wave/wfe/fetch/"+waveid+"/"+session+"?v=3",
+                     {'Cookie': "WAVE="+cookie}, None)
